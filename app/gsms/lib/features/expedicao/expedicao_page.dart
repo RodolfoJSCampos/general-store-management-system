b5a4e5109b61a48237ce63433c0af30d80e8b8f4
Rodolfo Campos <rodolfojscampos@gmail.com>
2025-12-23 14:20:16 -0300

    feat: display delivery forecast in order details modal

--- app/gsms/lib/features/expedicao/expedicao_page.dart
+++ app/gsms/lib/features/expedicao/expedicao_page.dart
@@ -1557,7 +1557,9 @@ class _ExpedicaoPageState extends State<ExpedicaoPage>
                                         padding: const EdgeInsets.all(10),
                                         decoration: BoxDecoration(
                                           color: Colors.blue.withOpacity(0.15),
-                                          borderRadius: BorderRadius.circular(10),
+                                          borderRadius: BorderRadius.circular(
+                                            10,
+                                          ),
                                         ),
                                         child: Icon(
                                           Icons.event_available_rounded,
